0fb9c7ba12a72f552e99da30473bad9c2347dd91
Alef Silva <alefsilva1809@gmail.com>
2025-10-22 21:52:23 -0300

    Add files via upload

---
+++ contador1.js
@@ -0,0 +1,7 @@
+function calcular(){
+    var num = parseFloat(document.getElementById("numero")).value;
+    //while cria a variavel que vai incrementar
+    while(1 <=10){
+}
+}
+//simoneneves2020
